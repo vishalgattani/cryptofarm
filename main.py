cd6c43fc4ccf370c4f7d7b7165539d90678a13c4
Vishal Gattani <vishalgattani09@gmail.com>
2023-06-03 02:09:17 -0700

    formatted

--- main.py
+++ main.py
@@ -128,108 +128,103 @@ def d_parser(s):
     return pd.to_datetime(s, format="%Y-%m-%d")
 
 
-try:
-    api_key = environ["CM_API_KEY"]
-    logging.info("Using API key found in environment")
-except KeyError:
-    api_key = ""
-    logging.info("API key not found. Using community client")
+if __name__ == "__main__":
+    try:
+        api_key = environ["CM_API_KEY"]
+        logging.info("Using API key found in environment")
+    except KeyError:
+        api_key = ""
+        logging.info("API key not found. Using community client")
 
-client = CoinMetricsClient(api_key)
-# starting_date = datetime.datetime(2009, 1, 3)
+    client = CoinMetricsClient(api_key)
+    # starting_date = datetime.datetime(2009, 1, 3)
 
+    btc_csv = Path("./btc.csv")
+    assert (
+        btc_csv.exists()
+    ), "download data from 'https://coinmetrics.io/community-network-data/'"
 
-btc_csv = Path("./btc.csv")
-assert (
-    btc_csv.exists()
-), "download data from 'https://coinmetrics.io/community-network-data/'"
+    def add_btc_info(df):
+        df["time"] = pd.to_datetime(df["time"]).dt.strftime("%Y-%m-%d")
+        df["BlkHeight"] = df.BlkCnt.cumsum()
+        df["Reward"] = 50 / (2 ** np.floor(df["BlkHeight"] / 210000))
+        df["BTCGenFrmBlk"] = df.BlkCnt * df["Reward"]
+        df["totalBTC"] = df.BTCGenFrmBlk.cumsum()
+        return df
 
+    df = pd.read_csv("btc.csv", low_memory=False)
+    df = add_btc_info(df)
 
-def add_btc_info(df):
-    df["time"] = pd.to_datetime(df["time"]).dt.strftime("%Y-%m-%d")
-    df["BlkHeight"] = df.BlkCnt.cumsum()
-    df["Reward"] = 50 / (2 ** np.floor(df["BlkHeight"] / 210000))
-    df["BTCGenFrmBlk"] = df.BlkCnt * df["Reward"]
-    df["totalBTC"] = df.BTCGenFrmBlk.cumsum()
-    return df
+    updated_btc_csv = Path("./btc_updated.csv")
+    if not updated_btc_csv.exists():
+        btc_supply_by_dates = []
+        my_API_fetcher = Coinmetrics_API()
+        for i in tqdm(range(len(df))):
+            btc_supply_by_dates.append(my_API_fetcher.btcSupplyOnDate(df.iloc[i].time))
+            time.sleep(0.5)
+        df["btc_supply_by_dates"] = btc_supply_by_dates
+        df.to_csv("btc_updated.csv")
 
+    # metrics = df.columns.to_list()[1:]
+    # last_date_in_data = df.time.iloc[-1]
+    # date_today = datetime.date.today()
+    # date_yesterday = date_today - datetime.timedelta(days=1)
 
-df = pd.read_csv("btc.csv", low_memory=False)
-df = add_btc_info(df)
+    # delta = date_yesterday - last_date_in_data   # returns timedelta
+    # for i in range(delta.days + 1):
+    #     day = last_date_in_data + datetime.timedelta(days=i)
+    #     print(day)
 
+    data = read_btc_data_from_csv("btc_updated.csv")
+    data = calculate_s2f(data, 463)
+    data = calculate_s2f(data, 10)
 
-updated_btc_csv = Path("./btc_updated.csv")
-if not updated_btc_csv.exists():
-    btc_supply_by_dates = []
-    my_API_fetcher = Coinmetrics_API()
-    for i in tqdm(range(len(df))):
-        btc_supply_by_dates.append(my_API_fetcher.btcSupplyOnDate(df.iloc[i].time))
-        time.sleep(0.5)
-    df["btc_supply_by_dates"] = btc_supply_by_dates
-    df.to_csv("btc_updated.csv")
-
-
-# metrics = df.columns.to_list()[1:]
-# last_date_in_data = df.time.iloc[-1]
-# date_today = datetime.date.today()
-# date_yesterday = date_today - datetime.timedelta(days=1)
-
-# delta = date_yesterday - last_date_in_data   # returns timedelta
-# for i in range(delta.days + 1):
-#     day = last_date_in_data + datetime.timedelta(days=i)
-#     print(day)
-
-
-data = read_btc_data_from_csv("btc_updated.csv")
-data = calculate_s2f(data, 463)
-data = calculate_s2f(data, 10)
-
-idx = pd.date_range("01-01-2009", "05-01-2032")
-# idx = pd.date_range('08-01-2010', '05-01-2032')
-result = data.copy(deep=True)
-result = result[
-    [
-        "time",
-        "BlkCnt",
-        "BlkHeight",
-        "Reward",
-        "CapRealUSD",
-        "PriceBTC",
-        "BTCGenFrmBlk",
-        "totalBTC",
-        "btc_supply_by_dates",
-        "s2f_ratio_463",
-        "s2f_ratio_usd_463",
-        "s2f_ratio_10",
-        "s2f_ratio_usd_10",
+    idx = pd.date_range("01-01-2009", "05-01-2032")
+    # idx = pd.date_range('08-01-2010', '05-01-2032')
+    result = data.copy(deep=True)
+    result = result[
+        [
+            "time",
+            "BlkCnt",
+            "BlkHeight",
+            "Reward",
+            "CapRealUSD",
+            "PriceBTC",
+            "BTCGenFrmBlk",
+            "totalBTC",
+            "btc_supply_by_dates",
+            "s2f_ratio_463",
+            "s2f_ratio_usd_463",
+            "s2f_ratio_10",
+            "s2f_ratio_usd_10",
+        ]
     ]
-]
-result = result.set_index(["time"])
-result = result.reindex(idx, fill_value=None)
-Printer.blue(result.columns)
-result.loc["2024-05-02"]["Reward"] = 3.125
-result.loc["2028-05-02"]["Reward"] = 1.5625
-result = result.bfill()
-result.Reward = result.Reward.ffill()
-result.BlkCnt = result.BlkCnt.bfill()
-result.BlkCnt = result.BlkCnt.fillna(144)
-result.BTCGenFrmBlk = result.BlkCnt * result.Reward
-result = result.reset_index()
-lastvalidindex = result["btc_supply_by_dates"].index.get_loc(
-    result["btc_supply_by_dates"].last_valid_index()
-)
-
-for i in range(lastvalidindex, len(result)):
-    result.iloc[i, result.columns.get_loc("btc_supply_by_dates")] = float(0)
-    result.iloc[i, result.columns.get_loc("btc_supply_by_dates")] = (
-        result.iloc[i - 1, result.columns.get_loc("btc_supply_by_dates")]
-        + result.iloc[i, result.columns.get_loc("BTCGenFrmBlk")]
+    result = result.set_index(["time"])
+    result = result.reindex(idx, fill_value=None)
+    Printer.blue(result.columns)
+    result.loc["2024-05-02"]["Reward"] = 3.125
+    result.loc["2028-05-02"]["Reward"] = 1.5625
+    result = result.bfill()
+    result.Reward = result.Reward.ffill()
+    result.BlkCnt = result.BlkCnt.bfill()
+    result.BlkCnt = result.BlkCnt.fillna(144)
+    result.BTCGenFrmBlk = result.BlkCnt * result.Reward
+    result = result.reset_index()
+    lastvalidindex = result["btc_supply_by_dates"].index.get_loc(
+        result["btc_supply_by_dates"].last_valid_index()
     )
 
-result = calculate_s2f(result, 463)
-result = calculate_s2f(result, 10)
+    for i in range(lastvalidindex, len(result)):
+        result.iloc[i, result.columns.get_loc("btc_supply_by_dates")] = float(0)
+        result.iloc[i, result.columns.get_loc("btc_supply_by_dates")] = (
+            result.iloc[i - 1, result.columns.get_loc("btc_supply_by_dates")]
+            + result.iloc[i, result.columns.get_loc("BTCGenFrmBlk")]
+        )
 
-print(result.describe())
+    result = calculate_s2f(result, 463)
+    result = calculate_s2f(result, 10)
 
-plt.plot(result["time"], result["PriceUSD"])
-plt.show()
+    print(result.describe())
+
+    plt.plot(result["time"], result["PriceUSD"])
+    plt.show()
